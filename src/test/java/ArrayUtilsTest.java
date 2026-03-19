7691b7569094a028ca01b9387911cca3597aba35
grotion <grotion0720@gmail.com>
2026-03-19 01:07:37 -0400

    Remove one test for testing

--- src/test/java/ArrayUtilsTest.java
+++ src/test/java/ArrayUtilsTest.java
@@ -41,8 +41,8 @@ public class ArrayUtilsTest {
         assertEquals(3, ArrayUtils.oddOrPos(new int[]{1, 2, 3}));
     }
 
-    @Test
-    public void testOddOrPositiveBothPositivesAndNegatives() {
-        assertEquals(3, ArrayUtils.oddOrPos(new int[]{-3, -2, 0, 1, 4}));
-    }
+//    @Test
+//    public void testOddOrPositiveBothPositivesAndNegatives() {
+//        assertEquals(3, ArrayUtils.oddOrPos(new int[]{-3, -2, 0, 1, 4}));
+//    }
 }
